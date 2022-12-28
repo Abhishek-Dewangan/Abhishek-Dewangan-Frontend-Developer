7afeda9308bbc2e5023557ed6cd64709a575ba63
callmeabhi1997 <callmeabhi1997@gmail.com>
2022-12-28 19:22:12 +0530

    Search form created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Banner} from './Components/Banner/Banner';
+import {SearchForm} from './Components/SearchForm/SearchForm';
 
 function App() {
   return (
     <div className='App'>
       <Banner />
+      <SearchForm />
     </div>
   );
 }
